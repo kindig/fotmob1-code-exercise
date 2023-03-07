44ddb2c3268dbb2d5555fb3cdbfc23cafdd57e0d
kindig <kindig@gmail.com>
2023-03-07 16:23:33 -0700

    Adding Extension.class

--- src/test/java/fotmob1.java
+++ src/test/java/fotmob1.java
@@ -1,9 +1,8 @@
-import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.extension.Extension;
 
-@ExtendWith()
+@ExtendWith(Extension.class)
 @CucumberOptions(
     features = {"src/test/resources/fotmob1/fotmob.feature"},
         glue = {"StepDefinitions"},
@@ -14,8 +13,7 @@ import org.junit.jupiter.api.extension.Extension;
             "junit:target/myreports/report.xml",
             "html:target/myreports/report.html"
         },
-        monochrome = true,
-        dryRun = false
+        monochrome = true
 )
 public class fotmob1 {
 }
